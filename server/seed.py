d21a472cf5cd36f44745958a74b44e69fe4b4dcf
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-24 00:51:34 +0300

    done....

--- server/seed.py
+++ server/seed.py
@@ -1,49 +1,27 @@
-#!/usr/bin/env python3
+from app import db
+from models import Book, Team
 
-from random import randint, choice as rc
+# Create and add some initial data to the database
+def seed_data():
+    with db.app.app_context():
+        db.create_all()
 
-from faker import Faker
+        # Add books (players)
+        book1 = Book(title='Cristiano Ronaldo', author='Al Nassr')
+        book2 = Book(title='Darwin Nunez', author='Liverpool FC')
+        book3 = Book(title='Valverde', author='Real Madrid')
 
-from app import app
-from models import db, Bakery, BakedGood
+        db.session.add(book1)
+        db.session.add(book2)
+        db.session.add(book3)
 
-fake = Faker()
+        # Add teams
+        team1 = Team(name='Al Nassr', player='Cristiano Ronaldo')
+        team2 = Team(name='Liverpool FC', player='Darwin Nunez')
+        team3 = Team(name='Real Madrid', player='Valverde')
 
-with app.app_context():
+        db.session.add(team1)
+        db.session.add(team2)
+        db.session.add(team3)
 
-    BakedGood.query.delete()
-    Bakery.query.delete()
-    
-    bakeries = []
-    for i in range(20):
-        b = Bakery(
-            name=fake.company()
-        )
-        bakeries.append(b)
-    
-    db.session.add_all(bakeries)
-
-    baked_goods = []
-    names = []
-    for i in range(200):
-
-        name = fake.first_name()
-        while name in names:
-            name = fake.first_name()
-        names.append(name)
-
-        bg = BakedGood(
-            name=name,
-            price=randint(1,10),
-            bakery=rc(bakeries)
-        )
-
-        baked_goods.append(bg)
-
-    db.session.add_all(baked_goods)
-    db.session.commit()
-    
-    most_expensive_baked_good = rc(baked_goods)
-    most_expensive_baked_good.price = 100
-    db.session.add(most_expensive_baked_good)
-    db.session.commit()
+        db.session.commit()
