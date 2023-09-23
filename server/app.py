dbb9317fce3e1588a2a42402fad465583c205abd
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-24 00:49:58 +0300

    done...

--- server/app.py
+++ server/app.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
-from flask_migrate import Migrate
-
-from models import db, Bakery, BakedGood
+from flask import Flask, jsonify
+from models import db, Book, Team
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return '<h1>Bakery GET API</h1>'
+@app.route('/players', methods=['GET'])
+def get_players():
+    players = Book.query.all()
+    player_list = [player.to_dict() for player in players]
+    return jsonify(player_list)
 
-@app.route('/bakeries')
-def bakeries():
-    return ''
-
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
-
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
-
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+@app.route('/teams', methods=['GET'])
+def get_teams():
+    teams = Team.query.all()
+    team_list = [team.to_dict() for team in teams]
+    return jsonify(team_list)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
